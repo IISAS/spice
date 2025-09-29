777ae5dec7e9ef298e15358497842a992df06fe2
stevo <stefan.dlugolinsky@savba.sk>
2025-09-29 14:38:26 +0200

    working

---
+++ ca/ca-clean.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "${SCRIPT_DIR}" && echo "🛈  CWD: ${PWD}"
+
+. ./envars.sh
+
+CA_HOME='./ca_home'
+echo "🛈  CA_HOME: $(realpath ${CA_HOME})"
+
+rm -fv ${CA_HOME}/*.pem
+rm -fv ${CA_HOME}/index.txt
+rm -fv ${CA_HOME}/index.txt.*
+touch ${CA_HOME}/index.txt && echo "created ${CA_HOME}/index.txt"
+
+rm -fv ${CA_HOME}/serial.txt
+rm -fv ${CA_HOME}/serial.txt.*
+echo 01 > ${CA_HOME}/serial.txt && echo "created ${CA_HOME}/serial.txt"
+
+rm -rfv ./volumes/certs/*
---
+++ ca/ca.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker \
+  run \
+  --rm \
+  -it \
+  -v "${SCRIPT_DIR}/volumes/etc/ssl:/etc/ssl" \
+  -v "${SCRIPT_DIR}/volumes/ca:/ca" \
+  -v "${SCRIPT_DIR}/volumes/certs:/certs" \
+  ca:latest \
+  $*
