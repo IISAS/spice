3ac56186dd2f53b6a16f325b810fe041b5350aef
stevo <stefan.dlugolinsky@savba.sk>
2025-09-30 07:24:16 +0200

    removed airflow and domino profiles

--- logs.sh
+++ logs.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-./cmd.sh --profile kafka --profile airflow --profile domino logs -f
+./cmd.sh \
+  --profile kafka \
+  logs -f
--- up.sh
+++ up.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 ./cmd.sh \
   --profile kafka \
-  --profile airflow \
-  --profile domino \
   up -d
