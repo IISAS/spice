b95cdef413dcc3463d7c4394d8b9c5d60093b197
stevo <stefan.dlugolinsky@savba.sk>
2025-10-01 12:06:17 +0200

    removed args for ca

--- ca/ca.sh
+++ ca/ca.sh
@@ -5,7 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 docker \
   run \
   --rm \
-  -it \
   -v "${SCRIPT_DIR}/volumes/etc/ssl:/etc/ssl" \
   -v "${SCRIPT_DIR}/volumes/ca:/ca" \
   -v "${SCRIPT_DIR}/volumes/certs:/certs" \
